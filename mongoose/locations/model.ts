f60a5ee4a7ea6639999e7fae621f9f64ed7df5bc
Emma Mikaelsdotter Andersson <emmamikaelsdotterandersson@Emmas-MacBook-Air.local>
2025-09-22 15:34:27 +0200

    Add location model

---
+++ mongoose/locations/model.ts
@@ -0,0 +1,6 @@
+import mongoose, { model } from "mongoose";
+
+import { LocationSchema, LocationType } from "mongoose/locations/schema";
+
+export default mongoose.models.locations ||
+  model<LocationType>("locations", LocationSchema);
